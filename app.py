74cb1737210eece2344f32cda0545bf87f647b10
Vinicius Gomes <viniciusgomes05@hotmail.com>
2023-10-13 02:07:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 """
-https://github.com/pydata/pandas-datareader/issues/170
+https://github.com/pydata/pandas-datareader/issues/170 -> onde encontrei uma solução utilizando o yfinance
 
-https://stackoverflow.com/questions/46922260/pycharm-pandas-datareader-not-found
+https://stackoverflow.com/questions/46922260/pycharm-pandas-datareader-not-found -> alguns erros ao utilizar o pandas-datareader
 """
+#importação de bibliotecas
 import datetime as dt
 import yfinance as yf
 import pandas_datareader.data as pdr
 import pandas as pd
 import streamlit as st
 
+#definição do primeiro e último dia
 end = dt.datetime.today()
 start = dt.datetime(end.year-1,end.month,end.day) #forma dinâmica de saber o dia
 
+#função do yfinance para corrigir os erros do pandas-datareader
 yf.pdr_override()
 
+#configurando a página
 st.set_page_config(page_title="Dashboard Financeiro",layout="wide")
 st.title('Dashboard Financeiro')
 
+#utilizando a estrutura with para criar os containers e colunas.
 with st.container():
     st.header('Insira as informações solicitadas abaixo :')
 
@@ -42,6 +47,7 @@ maior_cotacao = round(sp500['Adj Close'].max(),2) # maior cotacao do periodo
 prim_cotacacao = round(sp500.loc[sp500.index.min(),'Adj Close'],2) # primeira cotacao encontrada
 variacao = round(((ult_cotacao-prim_cotacacao)/prim_cotacacao)*100,2)
 
+#métricas no container
 with st.container():
     with col1:
         st.metric(f"última atualização - {ult_atualizacao}","RS {:,.2f}".format(ult_cotacao),f"{variacao}%")
@@ -50,6 +56,7 @@ with st.container():
     with col3:
         st.metric("Maior Cotação do período ","RS {:,.2f}".format(menor_cotacao))
 
+#gráficos
 with st.container():
     st.area_chart(sp500[['Adj Close']])
     st.line_chart(sp500[['Low','Adj Close','High']])
